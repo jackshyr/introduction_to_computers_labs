caf82baea4b81de92288263e58db77de23281a06
jackshyr <jackshyr@gmail.com>
2022-09-26 12:50:24 +0100

    09270812

--- lab03/test.py
+++ lab03/test.py
@@ -1,19 +1,19 @@
 # -*- coding: UTF-8 -*-
 number = input("please input a number:")#輸入數字 
 number = int(number)#將字串轉成數字
-if(number%2==1):
-    print("this is odd")
-else:
-    print("this is even")#判斷奇偶數
+if(number%2==1):#如果他是奇數
+    print("this is odd")#輸出他是奇數
+else:#其他
+    print("this is even")#輸出他是偶數
 a = input("please input your student ID first character:")#輸入開頭字母
 b = input("please input your student ID last 8 numbers:")#輸入後8位數字
 c  = int(b)#轉換成數字型態
-if(c%2==1):
-    print("your student ID is odd")
-else:
-    print("your student ID is even")#判斷奇偶數
+if(c%2==1):#如果他是奇數
+    print("your student ID is odd")#輸出他是奇數
+else:#其他
+    print("your student ID is even")#輸出他是偶數
 ID = a+b#將英文與數字組合
-print("your student ID is:")
-print(ID)#輸出ID
+print("your student ID is:"+ID)#輸出ID
+
 
 
