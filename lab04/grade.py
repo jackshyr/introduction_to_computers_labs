86bd7d6e38f6953adcf8b13cebc9bdae92a5c760
jackshyr <jackshyr@gmail.com>
2022-09-28 15:11:44 +0100

    09281011

--- lab04/grade.py
+++ lab04/grade.py
@@ -8,24 +8,24 @@ for i in range(0,3):
         A="B"
     else:
         A="C"
-    print("開始輸入"+A+"學生的成績,請依照 國文,英文,數學,自然,社會 的順序輸入:")
+    print("開始輸入"+A+"學生的成績,請依照 國文,英文,數學,自然,社會 的順序輸入:")#要求使用者輸入ABC三人的成績
     row =[]
     j = 0
     while j<5:
-        row.append(int(input()))
-        if row[j]>100 or row[j]<1:
+        row.append(int(input()))#讀入5科成績
+        if row[j]>100 or row[j]<1:#防止不合理成績
             j = j-1
             print("請輸入1-100的數值!")
             row.pop()
         j = j+1
-    l.append(row)
+    l.append(row)#建立二維陣列
     for k in range(0,5):
         sum = sum+ row[k]
-    sum = sum/5
-    ave.append(sum)
+    sum = sum/5#計算平均
+    ave.append(sum)#暫存平均
     print(A+"學生成績:")
-    print("國文: "+str(row[0])+",英文: "+str(row[1])+",數學: "+str(row[2])+",自然: "+str(row[3])+",社會: "+str(row[4])+"")
-    print()
+    print("國文: "+str(row[0])+",英文: "+str(row[1])+",數學: "+str(row[2])+",自然: "+str(row[3])+",社會: "+str(row[4])+"")#輸出五科成績
+    print()#換行
 for i in range(0,3):
     if i==0:
         S="A"
@@ -33,9 +33,9 @@ for i in range(0,3):
         S="B"
     else:
         S="C"
-    print(S+"學生平均成績 : "+str(ave[i]))
+    print(S+"學生平均成績 : "+str(ave[i]))#輸出三人平均
 print()
-for i in range(0,5):
+for i in range(0,5):#輸出五科平均
     if i==0:
         x="國文"
     elif i==1:
