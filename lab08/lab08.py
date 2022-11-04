6a8a6346cc9cfbd31822a47a4ff6da323955d6a0
jackshyr <jackshyr@gmail.com>
2022-11-04 12:41:11 +0000

    11040841

---
+++ lab08/lab08.py
@@ -0,0 +1,21 @@
+import os
+a = os.listdir(os.getcwd())#讀入當前目錄
+print(a)
+path = 'E94111229.txt'
+b = open (path,'w')
+c = 0
+for file in a:
+    b.write(a[c])
+    b.write(os.linesep)
+    c+=1
+c = 0
+path1 = '/home/E94111229'#讀入home目錄
+a = os.listdir(path1)
+print (a)
+b.write(os.linesep)
+for file in a:
+    b.write(a[c])
+    b.write(os.linesep)
+    c+=1
+b.close()
+
