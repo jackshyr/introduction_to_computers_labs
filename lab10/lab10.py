584e1fb011d91895483eb27c03cc7223fa236fa8
jackshyr <jackshyr@gmail.com>
2022-12-03 18:23:36 +0000

    12040223

--- lab10/lab10.py
+++ lab10/lab10.py
@@ -7,14 +7,14 @@ CORS(app)
 # web server 路由設定
 # 若有get request傳送到 / ，就會執行他下面的這個function，function名稱隨意，但不可重複
 @app.route('/',methods=['GET'])
-def root():
+def root(): #確定能收到get request
     return 'ok' # 發送response body 為 ok
 
 # 將webserver執行，監聽任意來源ip，port開在3000，開啟debug模式
 # debug模式代表，每次檔案更新後，webserver會自動重啟，不需要手動重啟
 dic0 ={}
 @app.route('/set',methods=['POST'])
-def root1():
+def root1(): #建立key-value pair
     data = request.form.to_dict()
     b = data.values()
     c = list(b)
@@ -24,18 +24,18 @@ def root1():
         dic0.update({c[0]:c[1]})
         return "set success"
 @app.route('/key_list',methods=['GET'])
-def root2():
+def root2(): #回傳存在的key
     a = dic0.keys()
     a = list(a)
     return str(a)
 @app.route('/get_value/<key>',methods=['GET'])
-def root3(key):
+def root3(key): #根據給的key回傳value
     if key in dic0:
         return dic0.get(key)
     else:
         return"key not found"
 @app.route('/update_value',methods=['POST'])
-def root4():
+def root4(): #更新value
     data1 = request.form.to_dict()
     b = data1.values()
     c = list(b)
@@ -45,7 +45,7 @@ def root4():
     else:
         return "key does not exist"
 @app.route('/delete/<key>',methods=['GET'])
-def root5(key):
+def root5(key): #刪除key-value
     if key in dic0:
         del dic0[key]
         return "delete success"
--- lab10/lab10_plus.py
+++ lab10/lab10_plus.py
@@ -9,12 +9,12 @@ CORS(app)
 # 若有get request傳送到 / ，就會執行他下面的這個function，function名稱隨意，但不可重複
 path = 'DATA.txt'
 @app.route('/',methods=['GET'])
-def root():
+def root(): #確定能收到get request
     return 'ok' # 發送response body 為 ok
 
 # 將webserver執行，監聽任意來源ip，port開在3000，開啟debug模式
 # debug模式代表，每次檔案更新後，webserver會自動重啟，不需要手動重啟
-f=open(path,"r")
+f=open(path,"r") #傳送txt的內容到字典
 content = f.read()
 cc = content.split()
 f.close()
@@ -23,37 +23,37 @@ dic0 ={}
 for j in range(0,l,2):
     dic0.update({cc[j]:cc[j+1]})
 @app.route('/set',methods=['POST'])
-def root1():
+def root1(): #建立key-value pair
     data = request.form.to_dict()
     b = data.values()
     c = list(b)
     if c[0] in dic0:
         return "key exist"
     else:
-        dic0.update({c[0]:c[1]})
+        dic0.update({c[0]:c[1]}) #將上傳到dic的內容傳給txt
         f = open(path, 'a')
         f.write(c[0]+" "+c[1])
         f.write(os.linesep)
         f.close()
         return "set success"
 @app.route('/key_list',methods=['GET'])
-def root2():
+def root2(): #回傳存在的key
     a = dic0.keys()
     a = list(a)
     return str(a)
 @app.route('/get_value/<key>',methods=['GET'])
-def root3(key):
+def root3(key): #根據給的key回傳value
     if key in dic0:
         return dic0.get(key)
     else:
         return"key not found"
 @app.route('/update_value',methods=['POST'])
-def root4():
+def root4(): #更新value
     data1 = request.form.to_dict()
     b = data1.values()
     c = list(b)
     if c[0] in dic0:
-        f=open(path,"r")
+        f=open(path,"r") #置換value的值到txt
         content=f.read()
         content=content.replace(c[0]+" "+dic0.get(c[0]),c[0]+" "+c[1])
         f.close()
@@ -65,9 +65,9 @@ def root4():
     else:
         return "key does not exist"
 @app.route('/delete/<key>',methods=['GET'])
-def root5(key):
+def root5(key): #刪除key-value
     if key in dic0:
-        f=open(path,"r")
+        f=open(path,"r") #刪除指定的key-value(雖然方法很蠢)
         content=f.read()
         content=content.replace(key+" "+dic0.get(key),"")
         f.close()
